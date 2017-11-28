754ceb090cf662acd0050cacd92abfe53d8e29b8
Josh Leverette <josh.leverette@utiliflex.com>
2017-11-28 09:23:06 -0500

    refactor a little

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,7 @@ extern crate time;
 
 use clap::{App, Arg};
 
-use std::io::{stdout, Read, Write};
-use std::path::PathBuf;
-use std::fs;
+use std::io::{stdout, Write};
 use std::thread::sleep;
 use std::time::Duration;
 use std::error::Error;
@@ -25,129 +23,10 @@ const APP_INFO: AppInfo = AppInfo {
     author: "Josh Leverette",
 };
 
-#[derive(Debug)]
-struct StrError(String);
+mod errors;
 
-impl std::convert::From<reqwest::Error> for StrError {
-    fn from(error: reqwest::Error) -> Self {
-        StrError(format!("{:#?}", error))
-    }
-}
-
-impl std::convert::From<serde_json::Error> for StrError {
-    fn from(error: serde_json::Error) -> Self {
-        StrError(format!("{:#?}", error))
-    }
-}
-
-impl std::convert::From<std::io::Error> for StrError {
-    fn from(error: std::io::Error) -> Self {
-        StrError(format!("{:#?}", error))
-    }
-}
-
-impl std::convert::From<String> for StrError {
-    fn from(error: String) -> Self {
-        StrError(error)
-    }
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Currency {
-    id: String,
-    name: String,
-    symbol: String,
-    rank: String,
-
-    price_usd: Option<String>,
-    price_btc: Option<String>,
-
-    #[serde(rename = "24h_volume_usd")] volume_usd_24h: Option<String>,
-
-    market_cap_usd: Option<String>,
-    available_supply: Option<String>,
-    total_supply: Option<String>,
-    percent_change_1: Option<String>,
-    percent_change_24: Option<String>,
-    percent_change_7: Option<String>,
-    last_updated: Option<String>,
-}
-
-fn fetch_ticker(
-    name: &str,
-    cache_file: Option<PathBuf>,
-    debug: bool,
-) -> Result<Currency, StrError> {
-    if debug {
-        println!("retrieving latest for {}", name);
-    }
-    let url = "https://api.coinmarketcap.com/v1/ticker/".to_string() + &name;
-    let mut resp = reqwest::get(url.as_str())?;
-    if !resp.status().is_success() {
-        return Err(format!("Ticker ID {} not valid.", name))?;
-    }
-
-    let mut content = String::new();
-    resp.read_to_string(&mut content)?;
-
-    let mut tickers: Vec<Currency> = serde_json::from_str(&content)?;
-
-    let ticker = tickers.remove(0);
-
-    if let Some(cache_file) = cache_file {
-        if debug {
-            println!("{} stored in cache", cache_file.display());
-        }
-        let file = fs::File::create(cache_file)?;
-        serde_json::to_writer(file, &ticker)?;
-    }
-
-    Ok(ticker)
-}
-
-fn print_ticker(name: String, cache: bool, debug: bool) -> Result<(), StrError> {
-    let cache_dir = app_root(AppDataType::UserCache, &APP_INFO)
-        .expect("Could not find or create the cache directory");
-
-    let ticker: Currency = if !cache {
-        fetch_ticker(&name, None, debug)?
-    } else {
-        let cache_file = cache_dir.join(format!("{}{}", name, ".json"));
-        let metadata = fs::metadata(&cache_file);
-        match metadata {
-            Ok(metadata) => match metadata.modified().unwrap().elapsed() {
-                Ok(elapsed) if elapsed < Duration::from_secs(1800) => {
-                    if debug {
-                        println!(
-                            "{} pulled from cache, {} seconds left until cache goes cold.",
-                            cache_file.display(),
-                            (Duration::from_secs(1800) - elapsed).as_secs()
-                        );
-                    }
-                    let file = fs::File::open(cache_file)?;
-                    serde_json::from_reader(file)?
-                }
-                _ => fetch_ticker(&name, Some(cache_file), debug)?,
-            },
-            _ => fetch_ticker(&name, Some(cache_file), debug)?,
-        }
-    };
-
-    let price = ticker.price_usd.unwrap_or("null".to_string());
-
-    let short_name;
-    if name == "ethereum" {
-        short_name = "eth".to_string();
-    } else if name == "bitcoin" {
-        short_name = "btc".to_string();
-    } else {
-        short_name = name;
-    }
-
-    print!("{}:{} ", short_name, price);
-
-    return Ok(());
-}
+mod ticker;
+use ticker::print_ticker;
 
 fn main() {
     let matches = App::new("cryptoticker")
